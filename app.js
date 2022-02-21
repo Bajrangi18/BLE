aa8667ef828b64dbbda70a75fe503da0e855719b
Bharatdeep Hazarika <67242026+Bajrangi18@users.noreply.github.com>
2022-02-22 01:34:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 const switcher = document.querySelector('.btn');
 switcher.addEventListener('click', function() {
-//   navigator.bluetooth.requestDevice({
-//   filters: [{
-//     name: 'ESP32NEW'
-//   }],
-//   optionalServices: ['4fafc201-1fb5-459e-8fcc-c5c9c331914b'] // Required to access service later.
-// })
-// .then(device => { /* … */ })
-// .catch(error => { console.error(error); });
-navigator.bluetooth.requestDevice({
-  acceptAllDevices: true,
+  navigator.bluetooth.requestDevice({
+  filters: [{
+    name: 'ESP32NEW'
+  }],
   optionalServices: ['4fafc201-1fb5-459e-8fcc-c5c9c331914b'] // Required to access service later.
 })
-.then(device => { /* … */ })
+.then(device => {
+     console.log(device.name);
+
+     return device.gatt.connect();
+})
+.then(characteristic => {
+  // Reading Battery Level…
+  return characteristic.readValue();
+})
+.then(value => {
+  document.getElementById('boldStuff').innerHTML = value;
+
+})
 .catch(error => { console.error(error); });
+
 });
