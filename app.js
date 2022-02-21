fa9330d86d4592a46cb39c6edf5c3a2965430026
Bharatdeep Hazarika <67242026+Bajrangi18@users.noreply.github.com>
2022-02-22 00:59:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 const switcher = document.querySelector('.btn');
 switcher.addEventListener('click', function() {
-//   navigator.bluetooth.requestDevice({
-//   filters: [{
-//     name: 'ESP32NEW'
-//   }],
-//   optionalServices: ['4fafc201-1fb5-459e-8fcc-c5c9c331914b'] // Required to access service later.
-// })
-// .then(device => { /* … */ })
-// .catch(error => { console.error(error); });
-navigator.bluetooth.requestDevice({
-  acceptAllDevices: true,
+  navigator.bluetooth.requestDevice({
+  filters: [{
+    name: 'ESP32NEW'
+  }],
   optionalServices: ['4fafc201-1fb5-459e-8fcc-c5c9c331914b'] // Required to access service later.
 })
 .then(device => { /* … */ })
 .catch(error => { console.error(error); });
+// navigator.bluetooth.requestDevice({
+//   acceptAllDevices: true,
+//   optionalServices: ['4fafc201-1fb5-459e-8fcc-c5c9c331914b'] // Required to access service later.
+// })
+// .then(device => { /* … */ })
+// .catch(error => { console.error(error); });
 });
